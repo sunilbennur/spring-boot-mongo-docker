b7e831e8552947546d253d5aa1c935871fa16ada
sunil rathod <sunilbennur25@gmail.com>
2022-10-17 15:54:42 +0530

    hel

--- initScript.sh
+++ initScript.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#hello
->>>>>>> refs/remotes/origin/master
 export MONGO_DB_USERNAME=${MONGO_DB_USERNAME:=`cat ${MONGO_DB_USERNAME_FILE}`}
 export MONGO_DB_PASSWORD=${MONGO_DB_PASSWORD:=`cat ${MONGO_DB_PASSWORD_FILE}`}
 java  -jar ./spring-boot-mongo.jar
